ff40f11cda0dc1933508cbdbc7b2d46bec6fb1a6
Xecozz <gillecamille@gmail.com>
2025-02-12 10:01:25 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+const posts = ref([]);
+const text = ref("hello");
+function addPost() {
+  posts.value.push(text.value);
+  text.value = "";
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <h1 id="titre" class="crevette">Bonjour</h1>
+    <div class="container">
+      <form class="card" @submit.prevent="addPost">
+        <textarea name="post" id="post" placeholder="saissiez" v-model="text"></textarea>
+        <button type="submit">Publier</button>
+      </form>
+      <p v-for="(post, index) in posts" :key="index">{{ post }}</p>
+    </div>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
